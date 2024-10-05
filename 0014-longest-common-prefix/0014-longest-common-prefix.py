09137029b53ba945c0305e323b32e83016691ba6
Szymon Gaweł <152923314+gawelszymon@users.noreply.github.com>
2024-10-05 19:51:29 +0200

    Time: 46 ms (8.16%), Space: 16.7 MB (5.41%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+                
+        pl = []
+        
+        if len(strs) == 1:
+            return strs[0]
+        
+        elif len(strs) > 1 and len(strs[0]) > 0 and len(strs[1]) > 0 and strs[0][0] != strs[1][0]:
+            return ""
+        
+        else:
+        
+            for l in range (len(strs[0])):
+                pl.append(strs[0][l])
+                for w in range (len(strs)):
+                    if w < len(strs) and l < len(strs[w]) and strs[w][l] == pl[l]:
+                        continue
+                    else:
+                        removedElement = strs[0][l]
+                        for i in range(len(pl) - 1, -1, -1):
+                            if pl[i] == removedElement:
+                                print(pl)
+                                del pl[i]
+                                return ''.join(pl)
+                        
+                        
+        prefix = ''.join(pl)            
+        return(prefix)        
